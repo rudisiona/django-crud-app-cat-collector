7d59ca9a185748354cb8cfd3d71ccce63cf3a50c
rudisiona <rsmageo@gmail.com>
2025-10-06 20:56:31 -0700

    end of django tables

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -75,11 +75,10 @@ WSGI_APPLICATION = 'catcollector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'catcollector',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
